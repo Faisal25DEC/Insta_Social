7f50673c4da25b7017e3a88ce87e9753679f087f
Faisal25DEC <faisalmd25121999@gmail.com>
2023-10-28 17:37:07 +0530

    store created

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,1 +1,7 @@
-export const store = "store";
+import { applyMiddleware, legacy_createStore } from "redux";
+import thunk from "redux-thunk";
+import { combineReducers } from "redux";
+
+const middleware = applyMiddleware(thunk);
+const rootReducer = combineReducers({});
+export const store = legacy_createStore(rootReducer, middleware);
