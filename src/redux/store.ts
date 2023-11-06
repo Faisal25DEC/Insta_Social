665e20d778c62528a0aa84b84430a47613c3671f
Faisal25DEC <faisalmd25121999@gmail.com>
2023-11-06 13:42:33 +0530

    file updates

--- src/redux/store.ts
+++
@@ -1,7 +0,0 @@
-import { applyMiddleware, legacy_createStore } from "redux";
-import thunk from "redux-thunk";
-import { combineReducers } from "redux";
-import { searchUserReducer } from "./search_user/search_user.reducer";
-const middleware = applyMiddleware(thunk);
-const rootReducer = combineReducers({searchUserReducer});
-export const store = legacy_createStore(rootReducer, middleware);
